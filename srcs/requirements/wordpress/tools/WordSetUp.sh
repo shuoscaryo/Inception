c1c8e827b938d48bf390919b66c3491ca26a6743
shuoscaryo <shuoscaryo@gmail.com>
2024-09-28 00:12:11 +0200

    [main]<2024-09-28-00:12> quicksave

--- srcs/requirements/wordpress/tools/WordSetUp.sh
+++ srcs/requirements/wordpress/tools/WordSetUp.sh
@@ -1,60 +1,44 @@
 #!/bin/bash
 
-# install wordpress cli
+# Download WP-CLI and save it as /usr/local/bin/wp so it can be used as "wp stuff"
 if [ ! -f "/usr/local/bin/wp" ]; then
   curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar --silent
   chmod +x wp-cli.phar
   mv wp-cli.phar /usr/local/bin/wp
 fi
 
-# Verify WP-CLI installation
+# Exit if WP-CLI installation failed
 if ! wp --info > /dev/null 2>&1; then
   echo "WP-CLI installation failed."
   exit 1
 fi
 
+# Wait for MySQL to be ready
 while true; do
 	echo "Checking connection to MySQL..."
-	if mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_HOST -e "SELECT 1;"
+	if mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -h mariadb -e "SELECT 1;"
 	then
 		break
 	fi
 	echo "Connection failed, retrying in 5 seconds..."
 	sleep 5
 done
-
 echo "Connection to MySQL established!"
 
 # Wordpress setup
+# Delete the default wp-config.php file even if has been created by another docker container 
+rm -rf /wordpress/wp-config.php
+mkdir -p /wordpress /wordpress/wp-content/uploads
+chmod -R 755 /wordpress
+cd /wordpress
+wp core download
+wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb
+wp core install --url=$DOMAIN_NAME --title=Inception --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email="$ADMIN_EMAIL"
+wp theme install --activate hello-elementor
+wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_PASSWORD
+wp plugin install elementor --activate
 
-if [ -f /wordpress/wp-config.php ]; then
-  rm /wordpress/wp-config.php
-  echo "Wordpress config file already exists, removing it..."
-fi
-
-if [ ! -f /wordpress/wp-config.php ]; then
-
-  mkdir -p /wordpress
-
-  chmod -R 755 /wordpress
-
-  cd wordpress
-
-  wp core download --path=/wordpress
-
-  wp config create --path=/wordpress --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST
-
-  wp core install --url=$DOMAIN_NAME --title=Inception --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email="$ADMIN_EMAIL" --path=/wordpress
-
-  wp theme install --activate hello-elementor --path=/wordpress
-
-  wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_PASSWORD --path=/wordpress
-
-  wp plugin install elementor --activate
-
-
-  echo "Wordpress setup completed!"
-fi
+echo "Wordpress setup completed!"
 
 echo "Starting PHP-FPM..."
 php-fpm82 --nodaemonize
