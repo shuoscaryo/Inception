1144f8f2fb4afc5804c06b4313b4a70168234013
shuoscaryo <shuoscaryo@gmail.com>
2024-09-29 22:35:05 +0200

    [main]<2024-09-29-22:35> chore: Removed unnecesary lines in init file

--- srcs/requirements/mariadb/tools/MariaSetUp.sh
+++ srcs/requirements/mariadb/tools/MariaSetUp.sh
@@ -6,18 +6,13 @@ chmod 777 /var/lib/mysql
 mysql_install_db >/dev/null 2>&1
 
 if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
-	echo "CREATE DATABASE $MYSQL_DATABASE;
-CREATE USER $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-CREATE USER $MYSQL_USER@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'%' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-DROP USER 'root'@'localhost';
-CREATE USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;" > $MYSQL_INIT_FILE
+	echo "CREATE DATABASE '$MYSQL_DATABASE';
+CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON '$MYSQL_DATABASE'.* TO '$MYSQL_USER'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'new_password';
+FLUSH PRIVILEGES;" > '$MYSQL_INIT_FILE'
   echo "Starting MariDB server..."
-  mysqld_safe --init-file=$MYSQL_INIT_FILE >/dev/null 2>&1
+  mysqld_safe --init-file='$MYSQL_INIT_FILE' >/dev/null 2>&1
 else
   echo "Starting MariDB server..."
   mysqld_safe >/dev/null 2>&1
